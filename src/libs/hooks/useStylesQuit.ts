15de1e1ac8a2e6c7d0aa2d50a1ca0b401e4d05bb
nxus <150361945+DumbNoxx@users.noreply.github.com>
2025-06-10 12:25:08 -0400

    Removing sentences to debug useStylesQuit

--- src/libs/hooks/useStylesQuit.ts
+++ src/libs/hooks/useStylesQuit.ts
@@ -4,13 +4,11 @@ export const useStylesQuit = () => {
   const targetButtonRef = useRef<HTMLButtonElement | null>(null);
   const svgTargetRef = useRef<HTMLSpanElement | null>(null);
   const textRef = useRef<HTMLDivElement | null>(null);
-  console.log(targetButtonRef.current, svgTargetRef.current, textRef.current)
   const handleClick = useCallback(() => {
     const svg = svgTargetRef.current;
     const textDiv = textRef.current;
     if (!(textDiv && svg)) return;
-    console.log("hola")
-    textDiv.classList.toggle("visible")
+    textDiv.classList.toggle("visible");
     svg.classList.toggle("open");
   }, []);
   useEffect(() => {
@@ -18,11 +16,8 @@ export const useStylesQuit = () => {
     if (!button) return;
     button.addEventListener("click", handleClick);
     return () => button.removeEventListener("click", handleClick);
-  }, [handleClick]);
+  }, []);
 
-  useEffect(() => {
-    console.log("Refs after mount:", { targetButtonRef, svgTargetRef, textRef })
-  }, [])
   return {
     targetButtonRef,
     svgTargetRef,
