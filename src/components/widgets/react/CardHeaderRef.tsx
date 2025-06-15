b54f7309dbd917eab5aaf5c58917ad08742c3dc7
nxus <150361945+DumbNoxx@users.noreply.github.com>
2025-06-14 22:18:08 -0400

    moving navBarLeft to tag nav, and removing component not used(Sss.tsx)

--- src/components/widgets/react/CardHeaderRef.tsx
+++ src/components/widgets/react/CardHeaderRef.tsx
@@ -1,21 +1,13 @@
 import { useVisibilityTrigger } from "@libs/client";
 import { ButtonCheckWrapper } from "@widget/index";
-import { NavLeft } from "./navLeft";
 
 
 export const CardHeaderRef = () => {
-  const listNav = [
-    "cardHeader",
-    "cardProject",
-    "cardExp",
-    "footer"
-  ];
   const { targetRef, buttonRef } = useVisibilityTrigger();
   return (
     <>
       <div ref={targetRef}></div>
       <ButtonCheckWrapper reference={buttonRef} />
-      <NavLeft id="cardHeader" className="navLeft" listNav={listNav} />
     </>
   )
 }
