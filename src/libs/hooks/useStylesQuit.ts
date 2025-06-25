02a0d38c475468404580b915207df06781ae0add
Nxus <150361945+DumbNoxx@users.noreply.github.com>
2025-06-25 02:28:25 -0400

    Editing useStylesQuit temporary logic

--- src/libs/hooks/useStylesQuit.ts
+++ src/libs/hooks/useStylesQuit.ts
@@ -1,22 +1,46 @@
-import { useEffect, useRef, useCallback } from "react";
+import { useEffect, useRef, useCallback, useState } from "react";
 
 export const useStylesQuit = () => {
   const targetButtonRef = useRef<HTMLButtonElement | null>(null);
   const svgTargetRef = useRef<HTMLSpanElement | null>(null);
   const textRef = useRef<HTMLDivElement | null>(null);
+  const [isActive, setIsActive] = useState(false);
+  // This manipulation of the DOM is temporary
+  const close = (textDiv: HTMLDivElement, svg: HTMLSpanElement) => {
+    textDiv.classList.remove("visible");
+    svg.classList.remove("open");
+    setIsActive(false);
+  }
+  const open = (textDiv: HTMLDivElement, svg: HTMLSpanElement) => {
+    textDiv.classList.add("visible");
+    svg.classList.add("open");
+    setIsActive(true);
+  }
   const handleClick = useCallback(() => {
     const svg = svgTargetRef.current;
     const textDiv = textRef.current;
     if (!(textDiv && svg)) return;
-    textDiv.classList.toggle("visible");
-    svg.classList.toggle("open");
-  }, []);
+    const buttons = document.querySelectorAll(".container-details");
+    const svgs = document.querySelectorAll(".container-svg");
+    if (!isActive) {
+      for (const button of buttons) button?.classList.remove("visible");
+      for (const svg of svgs) svg?.classList.remove("open");
+    }
+    if (!isActive) {
+      setTimeout(() => {
+        open(textDiv, svg);
+      }, 300);
+    } else {
+      close(textDiv, svg)
+    }
+    setIsActive(!isActive);
+  }, [isActive]);
   useEffect(() => {
     const button = targetButtonRef.current;
     if (!button) return;
     button.addEventListener("click", handleClick);
     return () => button.removeEventListener("click", handleClick);
-  }, []);
+  }, [handleClick]);
 
   return {
     targetButtonRef,
