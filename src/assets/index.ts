777130489f664772c174ceb7807df9b20144af43
Nxus <150361945+DumbNoxx@users.noreply.github.com>
2025-12-07 15:03:16 -0400

    feat: change projects

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,3 +1,4 @@
 export { default as cashRegister } from "./img/cashRegister.png";
 export { default as creatureCodex } from "./img/creatureCodex.png";
 export { default as phoneValidator } from "./img/phoneValidator.png";
+export { default as pomohub } from "./img/pomohub.png";
