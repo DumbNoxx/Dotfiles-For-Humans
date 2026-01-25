bfadec81454876a98725f23b721bb1ce92063c7b
Nxus <150361945+DumbNoxx@users.noreply.github.com>
2026-01-25 13:07:28 -0400

    fix: bug scroll navleft

--- src/components/widgets/react/navLeft.tsx
+++ src/components/widgets/react/navLeft.tsx
@@ -1,17 +1,25 @@
 import type { NavLeftProps } from "@libs/index";
 import { useScrollActive } from "@libs/client";
 export const NavLeft = ({ id, className = "", listNav }: NavLeftProps) => {
-  const { itemsRef } = useScrollActive();
-  return (
-    <div className={className}>
-      {listNav?.map((id: string, index: number) => {
-        return (
-          <a href={`#${id}`} className={id} aria-label={`Move to ${id}`} ref={(el) => { itemsRef.current[index] = el }} key={index}>
-            <div className="buttonNav"></div>
-          </a>
-        )
-      })}
-    </div >
-  );
+	const { itemsRef } = useScrollActive();
+	return (
+		<div className={className}>
+			{listNav?.map((id: string, index: number) => {
+				return (
+					<a
+						id={`nav-${id}`}
+						href={`#${id}`}
+						className={id}
+						aria-label={`Move to ${id}`}
+						ref={(el) => {
+							itemsRef.current[index] = el;
+						}}
+						key={id}
+					>
+						<div className="buttonNav"></div>
+					</a>
+				);
+			})}
+		</div>
+	);
 };
-
--- src/libs/hooks/useScrollActive.ts
+++ src/libs/hooks/useScrollActive.ts
@@ -1,36 +1,37 @@
 import { useRef, useEffect, useCallback } from "react";
 
 export const useScrollActive = () => {
-  const itemsRef = useRef<Array<HTMLAnchorElement | null>>([]);
-  const handleScrollList = useCallback(() => {
-    if (typeof window === "undefined") return;
-    const items = itemsRef.current;
-    if (!items || items.length === 0) return;
-    const scrollPosition = window.scrollY + 100;
-    items.map((item) => {
-      if (!item) return;
-      const textId = item.href.indexOf("#");
-      if (textId === -1) return;
-      const id = item.href.substring(textId + 1);
-      const element = document.getElementById(id);
-      if (!element) return;
-      const startElement = element.offsetTop;
-      const heightElement = element.offsetHeight;
-      const endElement = startElement + heightElement;
-      const isActive = scrollPosition >= startElement && scrollPosition <= endElement;
-      Array.from(item.children).forEach(child => {
-        child.classList.toggle("active", isActive);
-      })
-    })
-  }, []);
-  useEffect(() => {
-    if (typeof window === "undefined") return;
-    window.addEventListener("scroll", handleScrollList);
-    handleScrollList();
-    return () => window.removeEventListener("scroll", handleScrollList);
-  }, [handleScrollList]);
+	const itemsRef = useRef<Array<HTMLAnchorElement | null>>([]);
+	const handleScrollList = useCallback(() => {
+		if (typeof window === "undefined") return;
+		const items = itemsRef.current;
+		if (!items || items.length === 0) return;
+		const scrollPosition = window.scrollY + 100;
+		items.forEach((item) => {
+			if (!item) return;
+			const textId = item.href.indexOf("#");
+			if (textId === -1) return;
+			const id = item.href.substring(textId + 1);
+			const element = document.getElementById(id);
+			if (!element) return;
+			const startElement = element.offsetTop;
+			const heightElement = element.offsetHeight;
+			const endElement = startElement + heightElement;
+			const isActive =
+				scrollPosition >= startElement && scrollPosition <= endElement;
+			Array.from(item.children).forEach((child) => {
+				child.classList.toggle("active", isActive);
+			});
+		});
+	}, []);
+	useEffect(() => {
+		if (typeof window === "undefined") return;
+		window.addEventListener("scroll", handleScrollList);
+		handleScrollList();
+		return () => window.removeEventListener("scroll", handleScrollList);
+	}, [handleScrollList]);
 
-  return {
-    itemsRef
-  }
-}
+	return {
+		itemsRef,
+	};
+};
