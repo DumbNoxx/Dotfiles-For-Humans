81001d990df99527e7fec67542149ac84430cef1
Nxus <150361945+DumbNoxx@users.noreply.github.com>
2026-01-25 12:43:02 -0400

    feat: Add goxe to project section

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,4 +1,5 @@
 export { default as cashRegister } from "./img/cashRegister.png";
 export { default as creatureCodex } from "./img/creatureCodex.png";
+export { default as goxe } from "./img/goxe.png";
 export { default as phoneValidator } from "./img/phoneValidator.png";
 export { default as pomohub } from "./img/pomohub.png";
