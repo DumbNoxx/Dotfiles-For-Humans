d53c5ab1b0706de1efb28a1e4227ffe60c82d1e5
Nxus <150361945+DumbNoxx@users.noreply.github.com>
2026-02-10 00:37:22 -0400

    add robot.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import { defineConfig } from "astro/config";
 import path from "path";
 
 import react from "@astrojs/react";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  site: "https://nxus.pages.dev",
+  integrations: [react(), sitemap()],
 });
