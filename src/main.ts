920dc7d45bc8fe236ac1e2cfa2bf1ff20b8825df
Nxus <nxusdev@protonmail.com>
2026-03-16 08:19:51 -0400

    test: add SEO

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { createHead } from '@unhead/vue';
 import ui from '@nuxt/ui/vue-plugin'
 
 const app = createApp(App)
+const head = createHead()
 
 app.use(router)
 
 app.use(ui)
+app.use(head)
 
 app.mount('#app')
