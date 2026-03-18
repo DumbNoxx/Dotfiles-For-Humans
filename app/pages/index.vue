8bfa67d3e3925bec938f58071c5e3058027f9ac7
Nxus <nxusdev@protonmail.com>
2026-03-18 00:16:46 -0400

    sdfasdf

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -30,10 +30,10 @@ async function fetchGithubData() {
     }
 }
 onMounted(fetchGithubData)
-useMetaSeo({
+useSeoMeta({
   title: "Dylan Marcano | Software Developer",
   ogUrl: "https://nxus.pages.dev",
-  ogTitle:"Dylan Marcano | Software Developer", 
+  ogTitle:"Dylan Marcano | Software Developer",  
   description: "Explore my work: a Software Developer merging cutting-edge technologies with bussiness-driven results.",
   ogDescription:"Explore my work: a Software Developer merging cutting-edge technologies with bussiness-driven results.",
   twitterCard: "sumary",
