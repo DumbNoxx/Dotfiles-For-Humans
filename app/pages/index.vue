5ce90f866685c31e25c272cb9d02b3d10502aece
Nxus <nxusdev@protonmail.com>
2026-03-17 23:55:36 -0400

    fix: metatag to index

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -30,6 +30,17 @@ async function fetchGithubData() {
     }
 }
 onMounted(fetchGithubData)
+useMetaSeo({
+  title: "Dylan Marcano | Software Developer",
+  ogUrl: "https://nxus.pages.dev",
+  ogTitle:"Dylan Marcano | Software Developer" 
+  description: "Explore my work: a Software Developer merging cutting-edge technologies with bussiness-driven results.",
+  ogDescription:"Explore my work: a Software Developer merging cutting-edge technologies with bussiness-driven results.",
+  twitterCard: "sumary",
+  twitterTitle: "Dylan Marcano | Software Developer",
+  twitterDescription: "Explore my work: a Software Developer merging cutting-edge technologies with bussiness-driven results."
+
+})
 </script>
 
 <template>
