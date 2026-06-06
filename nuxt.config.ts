7119295d8e370420ca8e098dd055f176c02f3b7a
Nxus <nxusdev@protonmail.com>
2026-06-06 16:12:52 -0400

    chore: add script analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,17 @@
 export default defineNuxtConfig({
     compatibilityDate: '2025-07-15',
     devtools: { enabled: true },
-    modules: ["@nuxt/ui", "@nuxtjs/sitemap" ],
+    modules: ["@nuxt/ui", "@nuxtjs/sitemap"],
     app: {
         viewTransition: true,
         head: {
+            script: [
+                {
+                    src: 'https://static.cloudflareinsights.com/beacon.min.js',
+                    defer: true,
+                    'data-cf-beacon': JSON.stringify({ token: '9851a764004d4b6cb5c39bdbe5dac3fa' })
+                }
+            ],
             titleTemplate: "%s",
             htmlAttrs: {
                 lang: "en"
